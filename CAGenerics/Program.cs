1a0d77f1dc850595408930a6d7d70dcd21ac2718
Ahmed ramadan <Ahmedr2304@gmail.com>
2025-04-03 07:34:06 +0300

    Create an example for Generic method

--- CAGenerics/Program.cs
+++ CAGenerics/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// References channels for explain 
+/// https://www.youtube.com/watch?v=xE6bS6EfKAc
+/// 
+
+#region Generic Method
+Print(1); // pass int as paramter
+Print("Ahmed"); // pass string as paramter
+Print(new {Fname ="Ahmed",LName="Ramadan"}); // Anonymous Object 
+/// Generic Method
+static void Print<T>(T Value)
+{
+    Console.WriteLine("******** Generic Method ***********");
+    Console.WriteLine($"Type Paramter : {typeof(T)}");
+    Console.WriteLine($"Value : {Value}");
+    Console.WriteLine("******** End Generic Method ***********");
+    Console.WriteLine("\n\n");
+}
+#endregion
